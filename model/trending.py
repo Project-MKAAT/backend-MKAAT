9bc075edfe0822bc722f681d8c294998bccdb2c5
KinetekEnergy <transyt8@gmail.com>
2024-05-28 21:38:09 -0700

    returns only the rating

--- model/trending.py
+++ model/trending.py
@@ -16,16 +16,26 @@ from datetime import date
 
 
 class Trending(db.Model):
-    __tablename__ = 'trendingAnime'
+    __tablename__ = "trendingAnime"
 
-    _name = db.Column(db.String(255), unique=False, nullable = False)
+    _name = db.Column(db.String(255), unique=False, nullable=False)
     _searches = db.Column(db.Integer, primary_key=True)
-    _genre = db.Column(db.String(255), unique=False, nullable = False)
-    _releaseDate = db.Column(db.Date, unique=False, nullable = False)
-    _rating = db.Column(db.Float, unique=False, nullable = False)
-    _userRating = db.Column(db.String(20), default=json.dumps([]), unique=False, nullable = False)
-    
-    def __init__(self, name="", searches=0, genre="", releaseDate=date.today(), rating=0, userRating=json.dumps([])):
+    _genre = db.Column(db.String(255), unique=False, nullable=False)
+    _releaseDate = db.Column(db.Date, unique=False, nullable=False)
+    _rating = db.Column(db.Float, unique=False, nullable=False)
+    _userRating = db.Column(
+        db.String(20), default=json.dumps([]), unique=False, nullable=False
+    )
+
+    def __init__(
+        self,
+        name="",
+        searches=0,
+        genre="",
+        releaseDate=date.today(),
+        rating=0,
+        userRating=json.dumps([]),
+    ):
         self._name = name
         self._searches = searches
         self.genre = genre
@@ -36,6 +46,7 @@ class Trending(db.Model):
     @property
     def name(self):
         return self._name
+
     @name.setter
     def name(self, name):
         self._name = name
@@ -91,49 +102,59 @@ class Trending(db.Model):
         except IntegrityError:
             db.session.remove()
             return None
+
     def read(self):
+        user_ratings = json.loads(self.userRating)
+        ratings = [list(rating.values())[0] for rating in user_ratings]
+
         return {
             "title": self.name,
             "popularity": self.searches,
             "genre": self.genre,
             "release": self.releaseDate,
             "rating": self.rating,
-            "userRating": self.userRating,
+            "userRating": ratings,
         }
+
+
 def fetchAnimeTitles():
     animeTitles = []
 
-    urls = [ 'https://myanimelist.net/topanime.php?limit=0' , 'https://myanimelist.net/topanime.php?limit=50' ] 
+    urls = [
+        "https://myanimelist.net/topanime.php?limit=0",
+        "https://myanimelist.net/topanime.php?limit=50",
+    ]
     for url in urls:
         response = requests.get(url)
 
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        blockquote_tags = soup.find_all('h3')
+        blockquote_tags = soup.find_all("h3")
 
         animeTags = [tag.get_text() for tag in blockquote_tags]
 
         for i in animeTags:
             animeTitles.append(i)
         animeTitles = [item for item in animeTitles if "More" not in item]
-    with open('animeTitles.txt', 'a') as file:
+    with open("animeTitles.txt", "a") as file:
         file.write(str(animeTitles))
+
 
 def getSearches():
     animeRelevancy = []
-    with open('animeTitles.txt', 'r') as file:
+    with open("animeTitles.txt", "r") as file:
         lines = file.readlines()
         stringAnime = lines[0]
 
-    listAnime = stringAnime.strip('][').split(', ')
+    listAnime = stringAnime.strip("][").split(", ")
     listAnime = [string.replace("'", "") for string in listAnime]
 
     def get_weekly_searches(keyword):
-        pytrends = TrendReq(hl='en-US', tz=360)
+        pytrends = TrendReq(hl="en-US", tz=360)
         kw_list = [keyword]
 
         # Fetch interest over time for the past 7 days
-        pytrends.build_payload(kw_list, cat=0, timeframe='now 1-d', geo='', gprop='')
+        pytrends.build_payload(kw_list, cat=0, timeframe="now 1-d", geo="", gprop="")
         data = pytrends.interest_over_time()
 
         # Check if the data is not empty
@@ -146,54 +167,64 @@ def getSearches():
     def summarize_searches(data, keyword):
         if data is not None:
             total_searches = data[keyword].sum()
-            return(total_searches)
+            return total_searches
 
     print(listAnime)
     while listAnime:
         try:
             for title in listAnime:
                 data = get_weekly_searches(title)
-                animeRelevancy.append(dict(name=str(title), searches=summarize_searches(data,title)))
-                with open('animes.txt', 'a') as file:
-                    file.write(str(dict(name=str(title), searches=summarize_searches(data,title))))
+                animeRelevancy.append(
+                    dict(name=str(title), searches=summarize_searches(data, title))
+                )
+                with open("animes.txt", "a") as file:
+                    file.write(
+                        str(
+                            dict(
+                                name=str(title),
+                                searches=summarize_searches(data, title),
+                            )
+                        )
+                    )
                 listAnime.remove(title)
-                with open('animeTitles.txt', 'w') as file:
+                with open("animeTitles.txt", "w") as file:
                     file.write(str(listAnime))
                 print(animeRelevancy)
         except requests.exceptions.HTTPError as http_err:
             if http_err.response.status_code == 429:
                 print("429 again... waiting")
-                time.sleep(30)            
+                time.sleep(30)
             else:
                 print("ERROR OTHER THAN HTTP! SHOOT")
                 break
-            
+
         except Exception as e:
             print(f"diff error lol: {e}")
             time.sleep(15)
 
+
 def initTrending():
     with app.app_context():
         # fetchAnimeTitles()
-        if 1!=1:
+        if 1 != 1:
             getSearches()
         db.create_all()
         names = []
         searche = []
         relevancy = []
-        with open('animes.txt','r') as file:
+        with open("animes.txt", "r") as file:
             stringAnime = file.readlines()[0]
-            stringAnime = re.sub(r'}{','},{', stringAnime)
+            stringAnime = re.sub(r"}{", "},{", stringAnime)
             stringAnime = "[" + stringAnime + "]"
             animeList = ast.literal_eval(stringAnime)
 
             print(animeList)
             data = [entry for entry in animeList if entry["searches"] is not None]
 
-            sorted_data = sorted(data, key=lambda x: x['searches'])    
+            sorted_data = sorted(data, key=lambda x: x["searches"])
         for entry in sorted_data:
-            names.append(entry['name'])
-            searche.append(entry['searches'])
+            names.append(entry["name"])
+            searche.append(entry["searches"])
 
         for i in range(len(names)):
             temp = Trending(name=names[i], searches=searche[i])
